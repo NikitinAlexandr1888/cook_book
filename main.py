5115c506082be8724273c169225a941b02926623
AlexandrN <nikitinalexandr1888@yandex.ru>
2024-01-05 11:01:46 +0300

    Add person

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ def my_cook_book():
             tmp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
         cook_book[name] = tmp
   return cook_book
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+  cook_book = my_cook_book()
+  shop_list = {}
+  for dish in dishes:
+    for ingredient in cook_book[dish]:
+      name = ingredient['ingredient_name']
+      quantity = ingredient['quantity'] * person_count
+      measure = ingredient['measure']
+      if name in shop_list:
+        shop_list[name]['quantity'] += quantity
+      else:
+        shop_list[name] = {'measure': measure, 'quantity': quantity}
+  return shop_list
